6ecf0b7cb49d06f5cdf818701ba88cff6b27ef43
Nathan <dynamonath@nathanjwtx.me>
2018-07-23 16:38:19 -0500

    Added animation to player
    Created basic Main scene with background and instance of Player.

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -3,9 +3,10 @@ using System;
 
 public class Player : Area2D
 {
-    // Member variables here, example:
-    // private int a = 2;
-    // private string b = "textvar";
+    [Export] public int Speed;
+    
+    public Vector2 Velocity;
+    public Vector2 Screensize = new Vector2(480, 720);
 
     public override void _Ready()
     {
@@ -14,10 +15,53 @@ public class Player : Area2D
         
     }
 
-//    public override void _Process(float delta)
-//    {
-//        // Called every frame. Delta is time since last frame.
-//        // Update game logic here.
-//        
-//    }
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+        GetInput();
+        Position += Velocity * delta;
+        var pos = Position;
+        pos.x = Mathf.Clamp(Position.x, 0, Screensize.x);
+        pos.y = Mathf.Clamp(Position.y, 0, Screensize.y);
+        AnimatedSprite aniSprite = (AnimatedSprite) GetNode("AnimatedSprite");
+        
+        if (Velocity.Length() > 0)
+        {
+            aniSprite.Animation = "run";
+            aniSprite.FlipH = Velocity.x < 0;
+        }
+        else
+        {
+            aniSprite.Animation = "idle";
+        }
+    }
+
+    public void GetInput()
+    {
+        Velocity = new Vector2();
+        if (Input.IsActionPressed("ui_left"))
+        {
+            Velocity.x += -1;
+        }
+
+        if (Input.IsActionPressed("ui_right"))
+        {
+            Velocity.x += 1;
+        }
+
+        if (Input.IsActionPressed("ui_up"))
+        {
+            Velocity.y += -1;
+        }
+
+        if (Input.IsActionPressed("ui_down"))
+        {
+            Velocity.y += 1;
+        }
+
+        if (Velocity.Length() > 0)
+        {
+            Velocity = Velocity.Normalized() * Speed;
+        }
+    } 
 }
